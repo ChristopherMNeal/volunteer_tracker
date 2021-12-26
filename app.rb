48af618b9b382c5e16106e6fe0c8f865734427b1
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-26 12:52:51 -0800

    add route and view for projects.erb

--- app.rb
+++ app.rb
@@ -4,3 +4,12 @@ require('./lib/project')
 require('./lib/volunteer')
 require('pry')
 also_reload('lib/**/*.rb')
+
+get ('/') do
+  redirect to('/projects')
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
