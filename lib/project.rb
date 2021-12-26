ddb10a80ed84dd2677edaa777b8f2bf8e13fc4fd
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-26 12:41:22 -0800

    add project#delete and pass test

--- lib/project.rb
+++ lib/project.rb
@@ -53,7 +53,9 @@ class Project
     DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
   end
 
-
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{@id};")
+  end
 
 
 end
